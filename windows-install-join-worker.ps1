b43f4a090681353024c0153b06a763301b3b611a
Zuldajri <w.matyas15@hotmail.fr>
2018-11-16 10:33:28 +0100

    Update windows-install-join-worker.ps1

--- windows-install-join-worker.ps1
+++ windows-install-join-worker.ps1
@@ -5,7 +5,7 @@
 Param(
   [switch] $SkipEngineUpgrade,
   [string] $ArtifactPath = ".",
-  [string] $DockerEngineURI = "https://dockermsft.blob.core.windows.net/dockercontainer/docker-18-03-1-ee-2.zip",
+  [string] $DockerEngineURI = "https://dockermsft.blob.core.windows.net/dockercontainer/docker-18-09-0.zip",
   [string] $USERNAME,
   [string] $PASSWORD,
   [string] $UCPURI,
@@ -48,12 +48,12 @@ function Join-Swarm ()
 
     # Get the required images to configure the local engine
 
-    docker image pull docker/ucp-agent-win:3.0.6
-    docker image pull docker/ucp-dsinfo-win:3.0.6
+    docker image pull docker/ucp-agent-win:3.1.0
+    docker image pull docker/ucp-dsinfo-win:3.1.0
 
     # Execute the local node configuration
 
-    docker container run --rm docker/ucp-agent-win:3.0.6 windows-script | powershell -noprofile -noninteractive -command 'Invoke-Expression -Command $input'
+    docker container run --rm docker/ucp-agent-win:3.1.0 windows-script | powershell -noprofile -noninteractive -command 'Invoke-Expression -Command $input'
 
     # Deactivate HTTPS cert check to allow REST access to UCP with self signed cert
 
