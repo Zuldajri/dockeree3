1b187d5cf05856452fa9a52c90a222d9f2ed8a7e
Zuldajri <w.matyas15@hotmail.fr>
2018-11-16 20:18:00 +0100

    Update linux-install-dockereeucp.sh

--- linux-install-dockereeucp.sh
+++ linux-install-dockereeucp.sh
@@ -74,69 +74,26 @@ echo $DOCKER_SUBSCRIPTION > /home/$UCP_ADMIN_USERID/docker_subscription.lic
 
 chmod 777 /home/$UCP_ADMIN_USERID/docker_subscription.lic
 
-# Install Azure Container Networking
+# Create the azure_ucp_admin.toml
+docker swarm init
+touch /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
+echo AZURE_CLIENT_ID = "$AZURE_CLIENT_ID" > /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
+echo AZURE_TENANT_ID = "$AZURE_TENANT_ID" >> /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
+echo AZURE_SUBSCRIPTION_ID = "$AZURE_SUBSCRIPTION_ID" >> /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
+echo AZURE_CLIENT_SECRET = "$AZURE_CLIENT_SECRET" >> /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
 
-PLUGIN_VERSION=v1.0.13
-CNI_VERSION=0.3.1
-CNI_BIN_DIR=/opt/cni/bin
-CNI_NETCONF_DIR=/etc/cni/net.d
+# Create the Secret and the Service
+docker secret create azure_ucp_admin.toml /home/$UCP_ADMIN_USERID/azure_ucp_admin.toml
 
-function usage
-{
-    printf "Installs azure-vnet CNI plugins.\n"
-    printf "Usage: install-cni-plugin version [cniVersion]\n"
-}
-
-if [ "$PLUGIN_VERSION" = "" ]; then
-    usage
-    exit 1
-fi
-
-if [ "$CNI_VERSION" = "" ]; then
-    CNI_VERSION=v0.4.0
-fi
-
-# Create CNI directories.
-printf "Creating CNI directories.\n"
-mkdir -p $CNI_BIN_DIR
-mkdir -p $CNI_NETCONF_DIR
-
-# Install ebtables.
-if [ ! -e /sbin/ebtables ]
-then
-    printf "Installing ebtables package..."
-    apt-get update
-    apt-get install -y ebtables
-    printf "done.\n"
-else
-    echo "Package ebtables is already installed."
-fi
-/sbin/ebtables --list > /dev/null
-
-# Install azure-vnet CNI plugins.
-printf "Installing azure-vnet CNI plugin version $PLUGIN_VERSION to $CNI_BIN_DIR..."
-/usr/bin/curl -sSL https://github.com/Azure/azure-container-networking/releases/download/$PLUGIN_VERSION/azure-vnet-cni-linux-amd64-$PLUGIN_VERSION.tgz > $CNI_BIN_DIR/azure-vnet.tgz
-tar -xzf $CNI_BIN_DIR/azure-vnet.tgz -C $CNI_BIN_DIR
-printf "done.\n"
-
-# Install azure-vnet CNI network configuration file.
-printf "Installing azure-vnet CNI network configuration file to $CNI_NETCONF_DIR..."
-mv $CNI_BIN_DIR/*.conflist $CNI_NETCONF_DIR
-printf "done.\n"
-
-# Install loopback plugin.
-printf "Installing loopback CNI plugin version $CNI_VERSION to $CNI_BIN_DIR..."
-/usr/bin/curl -sSL https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz > $CNI_BIN_DIR/cni.tgz
-tar -xzf $CNI_BIN_DIR/cni.tgz -C $CNI_BIN_DIR ./loopback
-printf "done.\n"
-
-# Cleanup.
-rm $CNI_BIN_DIR/*.tgz
-chown root:root $CNI_BIN_DIR/*
-
-printf "azure-vnet CNI plugin is successfully installed.\n"
-
-
+docker service create \
+  --mode=global \
+  --secret=azure_ucp_admin.toml \
+  --log-driver json-file \
+  --log-opt max-size=1m \
+  --env IP_COUNT=128 \
+  --name ipallocator \
+  --constraint "node.platform.os == linux" \
+  docker4x/az-nic-ips
 
 # Azure - GET PODCIDR & Set up Route Table
 AZ_REPO=$(lsb_release -cs)
@@ -146,17 +103,17 @@ sudo apt-get update
 sudo apt-get install azure-cli
 
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-az resource tag --tags pod-cidr=10.200.4.0/24 -g $RGNAME -n linuxWorker1 --resource-type "Microsoft.Compute/virtualMachines"
-az resource tag --tags pod-cidr=10.200.5.0/24 -g $RGNAME -n linuxWorker2 --resource-type "Microsoft.Compute/virtualMachines"
-az resource tag --tags pod-cidr=10.200.6.0/24 -g $RGNAME -n linuxWorker3 --resource-type "Microsoft.Compute/virtualMachines"
+az resource tag --tags pod-cidr=192.168.0.0/16 -g $RGNAME -n linuxWorker1 --resource-type "Microsoft.Compute/virtualMachines"
+az resource tag --tags pod-cidr=192.168.0.0/16 -g $RGNAME -n linuxWorker2 --resource-type "Microsoft.Compute/virtualMachines"
+az resource tag --tags pod-cidr=192.168.0.0/16 -g $RGNAME -n linuxWorker3 --resource-type "Microsoft.Compute/virtualMachines"
 
 PRIVATE_IP_ADDRESS=$(az vm show -d -g $RGNAME -n linuxWorker1 --query "privateIps" -otsv)
-POD_CIDR=$(az vm show -g $RGNAME --name linuxWorker1 --query "tags" -o tsv)
+POD_CIDR=192.168.0.0/16
 echo $PRIVATE_IP_ADDRESS $POD_CIDR
 
 az network route-table create -g $RGNAME -n kubernetes-routes
 az network vnet subnet update -g $RGNAME -n docker --vnet-name clusterVirtualNetwork --route-table kubernetes-routes
-az network route-table route create -g $RGNAME -n kubernetes-route-10-200-1-0-24 --route-table-name kubernetes-routes --address-prefix 10.200.4.0/24 --next-hop-ip-address $PRIVATE_IP_ADDRESS --next-hop-type VirtualAppliance
+az network route-table route create -g $RGNAME -n kubernetes-route-192-168-0-16 --route-table-name kubernetes-routes --address-prefix 192.168.0.0/16 --next-hop-ip-address $PRIVATE_IP_ADDRESS --next-hop-type VirtualAppliance
 
 
 # Create the /etc/kubernetes/azure.json
@@ -173,7 +130,7 @@ echo "location": "$LOCATION", >> /home/$UCP_ADMIN_USERID/azure.json
 echo "subnetName": "docker", >> /home/$UCP_ADMIN_USERID/azure.json
 echo "securityGroupName": "ucpManager-nsg", >> /home/$UCP_ADMIN_USERID/azure.json
 echo "vnetName": "clusterVirtualNetwork", >> /home/$UCP_ADMIN_USERID/azure.json
-echo "routeTableName": "kubernetes-route-10-200-1-0-24", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "routeTableName": "kubernetes-route-192-168-0-16", >> /home/$UCP_ADMIN_USERID/azure.json
 echo "primaryAvailabilitySetName": "ucpAvailabilitySet", >> /home/$UCP_ADMIN_USERID/azure.json
 echo "cloudProviderBackoff": false >> /home/$UCP_ADMIN_USERID/azure.json
 echo "cloudProviderBackoffRetries": 0, >> /home/$UCP_ADMIN_USERID/azure.json
