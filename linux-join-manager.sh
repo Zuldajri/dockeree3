869041c5b987db91cfc7139bec053fa54b9fb107
Zuldajri <w.matyas15@hotmail.fr>
2018-11-19 14:00:09 +0100

    Update linux-join-manager.sh

--- linux-join-manager.sh
+++ linux-join-manager.sh
@@ -29,7 +29,7 @@ apt-get install jq unzip -y
 sudo apt-get install sshpass
 
 # Send bundle.zip
-sudo sshpass -p $UCP_ADMIN_PASSWORD scp $UCP_ADMIN_USERID@10.0.1.4:/home/$UCP_ADMIN_USERID/bundle.zip /home/$UCP_ADMIN_USERID/bundle.zip 
+sudo sshpass -p $UCP_ADMIN_PASSWORD scp -o StrictHostKeyChecking=No $UCP_ADMIN_USERID@10.0.1.4:/home/$UCP_ADMIN_USERID/bundle.zip /home/$UCP_ADMIN_USERID/bundle.zip 
 
 # Download the user client bundle to extract the certificate and configure the cli for the swarm to join
 unzip /home/$UCP_ADMIN_USERID/bundle.zip && chmod +x /home/$UCP_ADMIN_USERID/env.sh && source /home/$UCP_ADMIN_USERID/env.sh
