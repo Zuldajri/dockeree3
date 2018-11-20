8a91199026a12a89a8f972da58c123e089d8a03e
Zuldajri <w.matyas15@hotmail.fr>
2018-11-20 17:28:44 +0100

    Update linux-install-dockereeucp.sh

--- linux-install-dockereeucp.sh
+++ linux-install-dockereeucp.sh
@@ -186,5 +186,6 @@ sleep 30s
 
 IP=$(kubectl describe pod nfs-server | grep IP: | awk 'NR==1 {print $2}')
 
+wget https://raw.githubusercontent.com/Zuldajri/DockerEE/master/default-storage.yaml -O /home/$UCP_ADMIN_USERID/default-storage.yaml
 
 echo $(date) " linux-install-ucp - End of Script"
