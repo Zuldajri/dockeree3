039ac8738cd9945ce786c3e55041f7cdcc5d76d0
Zuldajri <w.matyas15@hotmail.fr>
2018-11-20 13:09:13 +0100

    Update linux-install-dtr.sh

--- linux-install-dtr.sh
+++ linux-install-dtr.sh
@@ -23,7 +23,7 @@ echo $(date) " linux-install-dtr - Now start the DTR installation"
 
 #install DTR
 
-docker run --rm docker/dtr:2.5.6 install \
+docker run --rm docker/dtr:2.6.0 install \
       --ucp-url $UCP_PUBLIC_FQDN \
       --ucp-node "dtrmanager" \
       --dtr-external-url $DTR_PUBLIC_FQDN \
