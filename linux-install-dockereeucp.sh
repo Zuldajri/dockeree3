8eb122cbc30d200ddbeecf0966fd1fda0310952a
Zuldajri <w.matyas15@hotmail.fr>
2018-11-27 17:01:25 +0100

    Update linux-install-dockereeucp.sh

--- linux-install-dockereeucp.sh
+++ linux-install-dockereeucp.sh
@@ -76,14 +76,44 @@ sudo apt-get install azure-cli
 
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
 
-PRIVATE_IP_ADDRESS=$(az vm show -d -g $RGNAME -n linuxWorker1 --query "privateIps" -otsv)
+PRIVATE_IP_ADDRESS1=$(az vm show -d -g $RGNAME -n linuxWorker1 --query "privateIps" -otsv)
+PRIVATE_IP_ADDRESS2=$(az vm show -d -g $RGNAME -n linuxWorker2 --query "privateIps" -otsv)
 POD_CIDR=192.168.0.0/16
-echo $PRIVATE_IP_ADDRESS $POD_CIDR
+echo $PRIVATE_IP_ADDRESS1 $PRIVATE_IP_ADDRESS2 $POD_CIDR
 
 az network route-table create -g $RGNAME -n kubernetes-routes
 az network vnet subnet update -g $RGNAME -n docker --vnet-name clusterVirtualNetwork --route-table kubernetes-routes
-az network route-table route create -g $RGNAME -n kubernetes-route-192-168-0-0-16 --route-table-name kubernetes-routes --address-prefix 192.168.0.0/16 --next-hop-ip-address $PRIVATE_IP_ADDRESS --next-hop-type VirtualAppliance
+az network route-table route create -g $RGNAME -n kubernetes-route-192-168-0-0-16 --route-table-name kubernetes-routes --address-prefix 192.168.0.0/16 --next-hop-ip-address $PRIVATE_IP_ADDRESS1 --next-hop-ip-address $PRIVATE_IP_ADDRESS2 --next-hop-type VirtualAppliance
 
+# Create the /etc/kubernetes/azure.json
+sudo mkdir /etc/kubernetes
+touch /home/$UCP_ADMIN_USERID/azure.json
+echo { > /home/$UCP_ADMIN_USERID/azure.json
+echo "cloud": "AzurePublicCloud", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "tenantId": "$AZURE_TENANT_ID", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "subscriptionId": "$AZURE_SUBSCRIPTION_ID", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "aadClientId": "$AZURE_CLIENT_ID", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "aadClientSecret": "$AZURE_CLIENT_SECRET", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "resourceGroup": "$RGNAME", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "location": "$LOCATION", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "subnetName": "docker", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "securityGroupName": "ucpManager-nsg", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "vnetName": "clusterVirtualNetwork", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "routeTableName": "kubernetes-route-192-168-0-0-16", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "primaryAvailabilitySetName": "ucpAvailabilitySet", >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderBackoff": false >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderBackoffRetries": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderBackoffExponent": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderBackoffDuration": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderBackoffJitter": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderRatelimit": false, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderRateLimitQPS": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "cloudProviderRateLimitBucket": 0, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "useManagedIdentityExtension": false, >> /home/$UCP_ADMIN_USERID/azure.json
+echo "useInstanceMetadata": false >> /home/$UCP_ADMIN_USERID/azure.json
+echo } >> /home/$UCP_ADMIN_USERID/azure.json
+
+sudo mv /home/$UCP_ADMIN_USERID/azure.json /etc/kubernetes/
 
 
 # Create the docker_subscription.lic
