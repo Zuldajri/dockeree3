ccf73c62da4e47f1a220957db1523d87fd80dcac
Zuldajri <w.matyas15@hotmail.fr>
2019-03-19 22:24:00 +0100

    Update linux-join-manager.sh

--- linux-join-manager.sh
+++ linux-join-manager.sh
@@ -15,13 +15,6 @@ echo "UCP_ADMIN_PASSWORD=<Not Copied for obvious security reasons"
 
 # Get necessary packages to process the UCP Auth Token
 
-# wget http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/j/jq-1.5-1.el7.x86_64.rpm
-
-# CENTOS
-
-#yum install -y epel-release
-#yum install -y jq-1.5-1.el7.x86_64.rpm
-
 # UBUNTU
 apt-get install jq unzip -y
 
@@ -31,15 +24,10 @@ sudo apt-get install sshpass
 # Send bundle.zip
 sudo sshpass -p $UCP_ADMIN_PASSWORD scp -o StrictHostKeyChecking=No $UCP_ADMIN_USERID@10.0.1.4:/home/$UCP_ADMIN_USERID/bundle.zip /home/$UCP_ADMIN_USERID/bundle.zip 
 
-#sudo sshpass -p $UCP_ADMIN_PASSWORD ssh -o StrictHostKeyChecking=No $UCP_ADMIN_USERID@10.0.1.4 "$(docker node update --label-add com.docker.ucp.orchestrator.kubernetes=true "$(cat /home/$UCP_ADMIN_USERID/linuxWorker2ID)")"
-#sudo sshpass -p $UCP_ADMIN_PASSWORD ssh -o StrictHostKeyChecking=No $UCP_ADMIN_USERID@10.0.1.4 "$(docker node update --label-add com.docker.ucp.orchestrator.swarm=false "$(cat /home/$UCP_ADMIN_USERID/linuxWorker2ID)")"
-
-
 # Download the user client bundle to extract the certificate and configure the cli for the swarm to join
 unzip /home/$UCP_ADMIN_USERID/bundle.zip && chmod +x /var/lib/waagent/custom-script/download/0/env.sh && source /var/lib/waagent/custom-script/download/0/env.sh
 
 # Ask the UCP Controller to give us the docker join command to execute
-
 docker swarm join-token manager|sed '1d'|sed '1d'|sed '$ d'> /var/lib/waagent/custom-script/download/0/docker-managerswarmjoin
 unset DOCKER_TLS_VERIFY
 unset DOCKER_CERT_PATH
