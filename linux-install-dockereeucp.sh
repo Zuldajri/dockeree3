c7fd04a7b7782ac1f844ec3080b1ae27e80f5a96
Zuldajri <w.matyas15@hotmail.fr>
2019-07-23 18:47:40 +0200

    Update linux-install-dockereeucp.sh

--- linux-install-dockereeucp.sh
+++ linux-install-dockereeucp.sh
@@ -48,7 +48,7 @@ curl -fsSL ${DOCKEREE_DOWNLOAD_URL}/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 6D085F96
 sudo add-apt-repository "deb [arch=amd64] ${DOCKEREE_DOWNLOAD_URL}/ubuntu $(lsb_release -cs) stable-18.09"
 sudo apt-get update -y
-sudo apt-get install -y docker-ee=5:18.09.6~3-0~ubuntu-xenial docker-ee-cli=5:18.09.6~3-0~ubuntu-xenial containerd.io
+sudo apt-get install -y docker-ee=5:19.03.0~3-0~ubuntu-xenial docker-ee-cli=5:19.03.0~3-0~ubuntu-xenial containerd.io
 
 # Post Installation configuration (all Linux distros)
 
