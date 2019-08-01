47ea042d1eececebb28fe342532b26794cee2ce1
Zuldajri <w.matyas15@hotmail.fr>
2019-08-01 11:51:11 +0200

    Update windows-install-join-worker.ps1

--- windows-install-join-worker.ps1
+++ windows-install-join-worker.ps1
@@ -5,7 +5,7 @@
 Param(
   [switch] $SkipEngineUpgrade,
   [string] $ArtifactPath = ".",
-  [string] $DockerEngineURI = "https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-0.zip",
+  [string] $DockerEngineURI = "https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip",
   [string] $USERNAME,
   [string] $PASSWORD,
   [string] $UCPURI,
